7b0790d817f211a6fe737a9fec422bcb61147f15
Herman Moreno <herman.moreno@crowdint.com>
2013-04-04 09:39:31 -0600

    Remove something

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -5,7 +5,6 @@ describe 'mars rover kata' do
   let(:rover) { Rover.new(position: { x: 0, y: 0 }, direction: :n, world: world) }
 
   it 'should end up at (2, 2)' do
-    binding.pry
     expect{rover.move('ffrff')}.to change{rover.position}.from({x: 0, y: 0}).to({x: 2, y: 2})
   end
 end
